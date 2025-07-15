4c95079a2fb5636d8219f0949c4bf70c427381b3
Blake <blakem0201@gmail.com>
2025-07-15 02:57:35 +0000

    test 1

--- Bash-Scripting-Project/Blake_Miller_bash-scripting-project.sh
+++ Bash-Scripting-Project/Blake_Miller_bash-scripting-project.sh
@@ -2,3 +2,30 @@
 
 #Blake Miller
 #date created: 7/14/2025
+
+
+#commands for displaying hostname ip address uptime and kernal version
+
+Display_System_Info(){
+
+
+    hname = $(hostname)
+    ip = $(ip addr)
+    up = $(uptime)
+    kernal = $(uname -a)
+
+    echo "The hostname is $hname"
+    echo ip
+    echo up
+    echo kernal
+
+}
+
+
+main(){
+    Display_System_Info
+
+
+}
+
+main
