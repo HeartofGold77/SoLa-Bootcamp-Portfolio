c51e46be07d44ce6d6abeb212b7d34fb3b2ca4b9
Blake <blakem0201@gmail.com>
2025-07-15 04:50:30 +0000

    Finished project

--- Bash-Scripting-Project/Blake_Miller_bash-scripting-project.sh
+++ Bash-Scripting-Project/Blake_Miller_bash-scripting-project.sh
@@ -62,12 +62,80 @@ Disk_Usage(){
 
 }
 
+#this function lists the top 20 procees running that use the most memeory
 
-main(){
-    Display_System_Info
-    Disk_Usage
-    List_Users
+Process_List(){
+    process=$(top -b -o +%MEM | head -n 20)
+    echo ""
+    echo ""
+    echo "Process info"
+    echo "$process" 
+
 
 }
+#displays failed login attempts for user but does ask for 
+#password since sudo is used
+
+
+Failed_Logins(){
+    fail=$(sudo lastb)
+    echo ""
+    echo ""
+    echo "Failed login info"
+    echo "$fail" 
+
+
+}
+
+#Checks if multiple systems are active 
+
+Active_Systems(){
+
+    audit=$(sudo systemctl is-active adutitd.service)
+    echo ""
+    echo ""
+    echo ""
+    echo "aduitd is $audit"
+
+    cron=$(sudo systemctl is-active cron.service)
+    echo ""
+    echo ""
+    echo ""
+    echo "cron is $cron"
+
+    journal=$(sudo systemctl is-active systemd-journald.service)
+    echo ""
+    echo ""
+    echo ""
+    echo "systemd-journald is $journal"
+
+
+    ufw=$(sudo systemctl is-active ufw.service)
+    echo ""
+    echo ""
+    echo ""
+    echo "ufw is $ufw"
+
+    sys=$(ps -p 1 -o comm=)
+    echo ""
+    echo ""
+    echo ""
+    echo "$sys is currently running on the system"
+}
+
+
+main(){
+
+    now=$(date +"%Y-%m-%d_%H-%M-%S")
+
+    Display_System_Info >> ~/Desktop/report-$now.txt
+    Disk_Usage >> ~/Desktop/report-$now.txt
+    List_Users >> ~/Desktop/report-$now.txt
+    Process_List >> ~/Desktop/report-$now.txt
+    Failed_Logins >> ~/Desktop/report-$now.txt
+    Active_Systems >> ~/Desktop/report-$now.txt
+
+}
+
 
 main
